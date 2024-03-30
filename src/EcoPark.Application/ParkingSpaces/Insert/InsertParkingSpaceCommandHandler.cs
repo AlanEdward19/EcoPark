2c0b7537fa45f89d8501e20f3c0bd37d395055dc
Alan Oliveira - BDS DataSolution <aoliveira@bdsdatasolution.com.br>
2024-03-29 22:26:43 -0300

    feat: insert parkingSpace command and handler

---
+++ src/EcoPark.Application/ParkingSpaces/Insert/InsertParkingSpaceCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿namespace EcoPark.Application.ParkingSpaces.Insert;
+
+public class InsertParkingSpaceCommandHandler(DatabaseDbContext databaseDbContext) : IHandler<InsertParkingSpaceCommand, DatabaseOperationResponseViewModel>
+{
+    public async Task<DatabaseOperationResponseViewModel> HandleAsync(InsertParkingSpaceCommand command, 
+        CancellationToken cancellationToken)
+    {
+        DatabaseOperationResponseViewModel result;
+
+        try
+        {
+            ParkingSpaceModel parkingSpaceModel = new(command.LocationId, command.Floor, command.ParkingSpaceName,
+                command.IsOccupied, command.Type);
+
+            await databaseDbContext.ParkingSpaces.AddAsync(parkingSpaceModel, cancellationToken);
+
+            await databaseDbContext.SaveChangesAsync(cancellationToken);
+
+            result = new DatabaseOperationResponseViewModel("Post", EOperationStatus.Successful,
+                "Parking space was inserted successfully!");
+        }
+        catch (Exception e)
+        {
+            result = new DatabaseOperationResponseViewModel("Post", EOperationStatus.Failed, e.Message);
+        }
+
+        return result;
+    }
+}
