c27c134605f554eab1e3169e13a60d6c999d0a35
Alan Oliveira - BDS DataSolution <aoliveira@bdsdatasolution.com.br>
2024-03-29 22:29:06 -0300

    feat: update reservation command and handler [Just Template]

---
+++ src/EcoPark.Application/Reservations/Update/UpdateReservationCommand.cs
@@ -0,0 +1,6 @@
+﻿namespace EcoPark.Application.Reservations.Update;
+
+public class UpdateReservationCommand
+{
+    
+}
---
+++ src/EcoPark.Application/Reservations/Update/UpdateReservationCommandHandler.cs
@@ -0,0 +1,10 @@
+﻿namespace EcoPark.Application.Reservations.Update;
+
+public class UpdateReservationCommandHandler : IHandler<UpdateReservationCommand, DatabaseOperationResponseViewModel>
+{
+    public async Task<DatabaseOperationResponseViewModel> HandleAsync(UpdateReservationCommand command, 
+        CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
