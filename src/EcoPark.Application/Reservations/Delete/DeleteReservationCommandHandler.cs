e790218b845b48ae44264f395bede719afd7b1db
Alan Oliveira - BDS DataSolution <aoliveira@bdsdatasolution.com.br>
2024-03-29 22:23:31 -0300

    feat: delete reservations command and handler [Just template]

---
+++ src/EcoPark.Application/Reservations/Delete/DeleteReservationCommandHandler.cs
@@ -0,0 +1,10 @@
+﻿namespace EcoPark.Application.Reservations.Delete;
+
+public class DeleteReservationCommandHandler : IHandler<DeleteReservationCommand, DatabaseOperationResponseViewModel>
+{
+    public async Task<DatabaseOperationResponseViewModel> HandleAsync(DeleteReservationCommand command, 
+        CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
