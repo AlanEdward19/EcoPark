b97ddaa5b5a13e46bffc6ee394d526c886fa9070
Alan Oliveira - BDS DataSolution <aoliveira@bdsdatasolution.com.br>
2024-03-29 22:35:34 -0300

    feat: usings

---
+++ src/EcoPark.Domain/Usings.cs
@@ -0,0 +1,12 @@
+﻿global using System.ComponentModel.DataAnnotations.Schema;
+global using System.ComponentModel.DataAnnotations;
+global using System.Collections.ObjectModel;
+
+#region Domain
+
+global using EcoPark.Domain.Commons.Enums;
+global using EcoPark.Domain.Aggregates.Location.ParkingSpace;
+global using EcoPark.Domain.Commons.Base;
+global using EcoPark.Domain.DataModels;
+
+#endregion
