b9bd66d3add32e8022407d2f3888d1a9ede9ef03
Alan Oliveira - BDS DataSolution <aoliveira@bdsdatasolution.com.br>
2024-03-29 22:27:01 -0300

    feat: insert reservation command and handler [Just Template]

---
+++ src/EcoPark.Application/Reservations/Insert/InsertReservationCommand.cs
@@ -0,0 +1,6 @@
+﻿namespace EcoPark.Application.Reservations.Insert;
+
+public class InsertReservationCommand
+{
+    
+}
---
+++ src/EcoPark.Application/Reservations/Insert/InsertReservationCommandHandler.cs
@@ -0,0 +1,10 @@
+﻿namespace EcoPark.Application.Reservations.Insert;
+
+public class InsertReservationCommandHandler : IHandler<InsertReservationCommand, DatabaseOperationResponseViewModel>
+{
+    public async Task<DatabaseOperationResponseViewModel> HandleAsync(InsertReservationCommand command, 
+        CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
