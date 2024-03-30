896e7d0ec77c5dd7c7f1c0d4ac98511dc5b7507e
Alan Oliveira - BDS DataSolution <aoliveira@bdsdatasolution.com.br>
2024-03-29 22:31:16 -0300

    feat: reservation simplified and normal viewModel

---
+++ src/EcoPark.Application/Reservations/Models/ReservationSimplifiedViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace EcoPark.Application.Reservations.Models;
+
+public class ReservationSimplifiedViewModel(Guid cardId, Guid clientId, string reservationCode, EReservationStatus status, DateTime reservationDate, DateTime expirationDate)
+{
+    public Guid CardId { get; private set; } = cardId;
+    public Guid ClientId { get; private set; } = clientId;
+    public string ReservationCode { get; private set; } = reservationCode;
+    public string Status { get; private set; } = status.ToString();
+    public DateTime ReservationDate { get; private set; } = reservationDate;
+    public DateTime ExpirationDate { get; private set; } = expirationDate;
+}
---
+++ src/EcoPark.Application/Reservations/Models/ReservationViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace EcoPark.Application.Reservations.Models;
+
+public class ReservationViewModel(Guid cardId, Guid clientId, string reservationCode, EReservationStatus status, DateTime reservationDate, DateTime expirationDate, 
+        ParkingSpaceSimplifiedViewModel parkingSpace) : ReservationSimplifiedViewModel(cardId, clientId, reservationCode, status, reservationDate, expirationDate)
+{
+    public ParkingSpaceSimplifiedViewModel ParkingSpace { get; private set; } = parkingSpace;
+}
