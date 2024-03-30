25952a4ea52ed3788d68cfa5cace57703102dd9e
Alan Oliveira - BDS DataSolution <aoliveira@bdsdatasolution.com.br>
2024-03-29 22:30:23 -0300

    feat: client simplified and normal viewModel

---
+++ src/EcoPark.Application/Clients/Models/ClientSimplifiedViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace EcoPark.Application.Clients.Models;
+
+public class ClientSimplifiedViewModel(string email, string firstName, string lastName)
+{
+    public string Email { get; private set; } = email;
+    public string FirstName { get; private set; } = firstName;
+    public string LastName { get; private set; } = lastName;
+}
---
+++ src/EcoPark.Application/Clients/Models/ClientViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace EcoPark.Application.Clients.Models;
+
+public class ClientViewModel(string email, string firstName, string lastName, IEnumerable<CarViewModel>? cars) : ClientSimplifiedViewModel(email, firstName, lastName)
+{
+    public IEnumerable<CarViewModel>? Cars { get; private set; } = cars;
+}
