b8f7b60e786d260c6a66113d610410da1407778e
Alan Oliveira - BDS DataSolution <aoliveira@bdsdatasolution.com.br>
2024-03-29 22:29:24 -0300

    feat: update location command and handler

---
+++ src/EcoPark.Application/Locations/Update/UpdateLocationCommand.cs
@@ -0,0 +1,10 @@
+﻿namespace EcoPark.Application.Locations.Update;
+
+public class UpdateLocationCommand(string name, string address)
+{
+    public Guid LocationId { get; private set; }
+    public string Name { get; private set; } = name;
+    public string Address { get; private set; } = address;
+
+    public void SetLocationId(Guid locationId) => LocationId = locationId;
+}
---
+++ src/EcoPark.Application/Locations/Update/UpdateLocationCommandHandler.cs
@@ -0,0 +1,40 @@
+﻿namespace EcoPark.Application.Locations.Update;
+
+public class UpdateLocationCommandHandler(DatabaseDbContext databaseDbContext) : IHandler<UpdateLocationCommand, DatabaseOperationResponseViewModel>
+{
+    public async Task<DatabaseOperationResponseViewModel> HandleAsync(UpdateLocationCommand command, 
+        CancellationToken cancellationToken)
+    {
+        DatabaseOperationResponseViewModel result;
+
+        try
+        {
+            LocationModel? locationModel = await databaseDbContext.Locations
+                .FirstOrDefaultAsync(l => l.Id == command.LocationId, cancellationToken);
+
+            if (locationModel != null)
+            {
+                LocationAggregateRoot locationAggregate = new(locationModel);
+
+                locationAggregate.UpdateName(command.Name);
+                locationAggregate.UpdateAddress(command.Address);
+
+                locationModel.UpdateBasedOnAggregate(locationAggregate);
+
+                databaseDbContext.Locations.Update(locationModel);
+
+                await databaseDbContext.SaveChangesAsync(cancellationToken);
+
+                result = new("Patch", EOperationStatus.Successful, "Location updated successfully");
+            }
+            else
+                result = new("Patch", EOperationStatus.Failed, "No Location were found with this id");
+        }
+        catch (Exception e)
+        {
+            result = new("Patch", EOperationStatus.Failed, e.Message);
+        }
+
+        return result;
+    }
+}
