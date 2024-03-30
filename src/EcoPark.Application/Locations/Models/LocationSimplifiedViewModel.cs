e6535e17de36e19abbe71f89b239c4b63367de58
Alan Oliveira - BDS DataSolution <aoliveira@bdsdatasolution.com.br>
2024-03-29 22:30:40 -0300

    feat: location simplified and normal viewModel

---
+++ src/EcoPark.Application/Locations/Models/LocationSimplifiedViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace EcoPark.Application.Locations.Models;
+
+public class LocationSimplifiedViewModel(string name, string address)
+{
+    public string Name { get; private set; } = name;
+    public string Address { get; private set; } = address;
+}
---
+++ src/EcoPark.Application/Locations/Models/LocationViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace EcoPark.Application.Locations.Models;
+
+public class LocationViewModel(string name, string address, IEnumerable<ParkingSpaceSimplifiedWithoutLocationViewModel>? parkingSpaces) 
+    : LocationSimplifiedViewModel(name, address)
+{
+   public IEnumerable<ParkingSpaceSimplifiedWithoutLocationViewModel>? ParkingSpaces { get; private set; } = parkingSpaces;
+}
