88317e38f0ccbc3d8cd6ddf63b394420ef31d1b7
Alan Oliveira - BDS DataSolution <aoliveira@bdsdatasolution.com.br>
2024-03-29 22:29:13 -0300

    feat: update parkingSpace command and handler

---
+++ src/EcoPark.Application/ParkingSpaces/Update/UpdateParkingSpaceCommand.cs
@@ -0,0 +1,12 @@
+﻿namespace EcoPark.Application.ParkingSpaces.Update;
+
+public class UpdateParkingSpaceCommand(int? floor, string? parkingSpaceName, EParkingSpaceType? parkingSpaceType, bool? isOccupied)
+{
+    public Guid ParkingSpaceId { get; private set; }
+    public int? Floor { get; private set; } = floor;
+    public string? ParkingSpaceName { get; private set; } = parkingSpaceName;
+    public EParkingSpaceType? ParkingSpaceType { get; private set; } = parkingSpaceType;
+    public bool? IsOccupied { get; private set; } = isOccupied;
+
+    public void SetParkingSpaceId(Guid parkingSpaceId) => ParkingSpaceId = parkingSpaceId;
+}
---
+++ src/EcoPark.Application/ParkingSpaces/Update/UpdateParkingSpaceCommandHandler.cs
@@ -0,0 +1,42 @@
+﻿namespace EcoPark.Application.ParkingSpaces.Update;
+
+public class UpdateParkingSpaceCommandHandler(DatabaseDbContext databaseDbContext) : IHandler<UpdateParkingSpaceCommand, DatabaseOperationResponseViewModel>
+{
+    public async Task<DatabaseOperationResponseViewModel> HandleAsync(UpdateParkingSpaceCommand command,
+        CancellationToken cancellationToken)
+    {
+        DatabaseOperationResponseViewModel result;
+
+        try
+        {
+            ParkingSpaceModel? parkingSpaceModel = await databaseDbContext.ParkingSpaces
+                .FirstOrDefaultAsync(p => p.Id == command.ParkingSpaceId, cancellationToken);
+
+            if (parkingSpaceModel != null)
+            {
+                ParkingSpaceAggregate parkingSpaceAggregate = new(parkingSpaceModel);
+
+                parkingSpaceAggregate.UpdateFloor(command.Floor);
+                parkingSpaceAggregate.UpdateParkingSpaceName(command.ParkingSpaceName);
+                parkingSpaceAggregate.UpdateParkingSpaceType(command.ParkingSpaceType);
+                parkingSpaceAggregate.SetOccupied(command.IsOccupied);
+
+                parkingSpaceModel.UpdateBasedOnAggregate(parkingSpaceAggregate);
+
+                databaseDbContext.ParkingSpaces.Update(parkingSpaceModel);
+
+                await databaseDbContext.SaveChangesAsync(cancellationToken);
+
+                result = new("Patch", EOperationStatus.Successful, "Parking space updated successfully");
+            }
+            else
+                result = new("Patch", EOperationStatus.Failed, "No Parking space were found with this id");
+        }
+        catch (Exception e)
+        {
+            result = new("Patch", EOperationStatus.Failed, e.Message);
+        }
+
+        return result;
+    }
+}
