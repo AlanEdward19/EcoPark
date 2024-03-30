69aab8fec7c4b73d3e1c894a40b031257e8383ad
Alan Oliveira - BDS DataSolution <aoliveira@bdsdatasolution.com.br>
2024-03-29 22:31:32 -0300

    feat: clientAggregate and related classes

---
+++ src/EcoPark.Domain/Aggregates/Client/Car.cs
@@ -0,0 +1,24 @@
+﻿namespace EcoPark.Domain.Aggregates.Client;
+
+public class Car
+{
+    public Guid Id { get; private set; }
+    public string Plate { get; private set; }
+    public ECarType Type { get; private set; }
+    public string Model { get; private set; }
+    public string Color { get; private set; }
+    public string Brand { get; private set; }
+    public int Year { get; private set; }
+
+    public void UpdatePlate(string plate) => Plate = plate;
+
+    public void UpdateType(ECarType type) => Type = type;
+
+    public void UpdateModel(string model) => Model = model;
+
+    public void UpdateColor(string color) => Color = color;
+
+    public void UpdateBrand(string brand) => Brand = brand;
+
+    public void UpdateYear(int year) => Year = year;
+}
---
+++ src/EcoPark.Domain/Aggregates/Client/ClientAggregateRoot.cs
@@ -0,0 +1,61 @@
+﻿namespace EcoPark.Domain.Aggregates.Client;
+
+public class ClientAggregateRoot
+{
+    private List<Car> _cars = new();
+
+    public Guid Id { get; private set; }
+    public string Email { get; private set; }
+    private string Password { get; set; }
+    public string FirstName { get; private set; }
+    public string LastName { get; private set; }
+
+    public IReadOnlyCollection<Car> Cars => new ReadOnlyCollection<Car>(_cars);
+
+    public void AddCar(Car car) => _cars.Add(car);
+
+    public bool RemoveCar(Car car) => _cars?.Remove(car) ?? false;
+
+    public bool RemoveCarById(Guid carId)
+    {
+        var car = _cars?.FirstOrDefault(c => c.Id == carId);
+
+        return car != null && (_cars?.Remove(car) ?? false);
+    }
+
+    public void UpdateCar(Guid carId, string? plate, ECarType? type, string? model, string? color, string? brand, int? year)
+    {
+        var existingCar = _cars?.FirstOrDefault(c => c.Id == carId);
+
+        if (existingCar == null)
+            return;
+
+        if (!string.IsNullOrWhiteSpace(plate))
+            existingCar.UpdatePlate(plate);
+
+        if (type != null)
+            existingCar.UpdateType(type.Value);
+
+        if (!string.IsNullOrWhiteSpace(model))
+            existingCar.UpdateModel(model);
+
+        if (!string.IsNullOrWhiteSpace(color))
+            existingCar.UpdateColor(color);
+
+        if (!string.IsNullOrWhiteSpace(brand))
+            existingCar.UpdateBrand(brand);
+
+        if (year != null)
+            existingCar.UpdateYear(year.Value);
+    }
+
+    public void UpdateFirstName(string firstName) => FirstName = firstName;
+
+    public void UpdateLastName(string lastName) => LastName = lastName;
+
+    public void UpdateEmail(string email) => Email = email;
+
+    public void UpdatePassword(string password) => Password = password;
+
+    public string GetFullName() => $"{FirstName} {LastName}";
+}
