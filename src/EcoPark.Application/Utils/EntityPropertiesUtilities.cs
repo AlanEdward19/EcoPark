1ac151edbb45add37533ad6d0b9c7c576602f0f5
Alan Oliveira - BDS DataSolution <aoliveira@bdsdatasolution.com.br>
2024-03-29 22:23:51 -0300

    feat: utility for reading class properties for logging

---
+++ src/EcoPark.Application/Utils/EntityPropertiesUtilities.cs
@@ -0,0 +1,7 @@
+﻿namespace EcoPark.Application.Utils;
+
+public static class EntityPropertiesUtilities
+{
+    public static IEnumerable<string> GetEntityPropertiesAndValueAsIEnumerable<T>(T entity) =>
+        entity.GetType().GetProperties().Select(x => $"{x.Name} = {x.GetValue(entity)}");
+}
