ebff0cd2bf74298c13b36640c1337f37adebb3ab
Alan Oliveira - BDS DataSolution <aoliveira@bdsdatasolution.com.br>
2024-03-29 22:24:12 -0300

    feat: CRUD base viewModel for operationStatus

---
+++ src/EcoPark.Application/Commons/Models/DatabaseOperationResponseViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace EcoPark.Application.Commons.Models;
+
+public class DatabaseOperationResponseViewModel(string operation, EOperationStatus status, string message)
+{
+    public string Operation { get; set; } = operation;
+    public string Status { get; set; } = status.ToString();
+    public string Message { get; set; } = message;
+}
