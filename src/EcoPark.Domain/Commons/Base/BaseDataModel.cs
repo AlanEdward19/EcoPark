60252a52e89631e7d779049dab42f4b2ab3a0db1
Alan Oliveira - BDS DataSolution <aoliveira@bdsdatasolution.com.br>
2024-03-29 22:32:07 -0300

    feat: base dataModel and userModel

---
+++ src/EcoPark.Domain/Commons/Base/BaseDataModel.cs
@@ -0,0 +1,18 @@
+﻿namespace EcoPark.Domain.Commons.Base;
+
+public class BaseDataModel
+{
+    [Key]
+    public Guid Id { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+
+    public BaseDataModel()
+    {
+        DateTime now = DateTime.Now;
+        Id = Guid.NewGuid();
+
+        CreatedAt = now;
+        UpdatedAt = now;
+    }
+}
---
+++ src/EcoPark.Domain/Commons/Base/UserModel.cs
@@ -0,0 +1,10 @@
+﻿namespace EcoPark.Domain.Commons.Base;
+
+public class UserModel(string email, string password, string firstName, string lastName)
+    : BaseDataModel
+{
+    public string Email { get; private set; } = email;
+    public string Password { get; private set; } = password;
+    public string FirstName { get; private set; } = firstName;
+    public string LastName { get; private set; } = lastName;
+}
