6c5b53d191bd26243433080a1444fcaf4f0e32a2
Alan Oliveira - BDS DataSolution <aoliveira@bdsdatasolution.com.br>
2024-03-29 22:33:25 -0300

    feat: ioc

---
+++ src/EcoPark.Presentation/Configurations/IoC.cs
@@ -0,0 +1,16 @@
+﻿using EcoPark.Application;
+using EcoPark.Infrastructure;
+
+namespace EcoPark.Presentation.Configurations;
+
+public static class IoC
+{
+    public static IServiceCollection ConfigureIoC(this IServiceCollection services, IConfiguration configuration)
+    {
+        services
+            .ConfigureInfrastructure(configuration)
+            .ConfigureApplication();
+
+        return services;
+    }
+}
