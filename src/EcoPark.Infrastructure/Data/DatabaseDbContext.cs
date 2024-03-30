4579a83abba7058456c6f92cca4701b3b3f082fa
Alan Oliveira - BDS DataSolution <aoliveira@bdsdatasolution.com.br>
2024-03-29 22:33:11 -0300

    feat: databaseContext and configs

---
+++ src/EcoPark.Infrastructure/Data/DatabaseDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EcoPark.Infrastructure.Data;
+
+public partial class DatabaseDbContext : DbContext
+{
+    public DatabaseDbContext(DbContextOptions<DatabaseDbContext> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<ParkingSpaceModel>()
+            .HasMany(p => p.Reservations)
+            .WithOne(r => r.ParkingSpace)
+            .HasForeignKey(r => r.ParkingSpaceId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+
+}
---
+++ src/EcoPark.Infrastructure/Data/DatabaseDbContextExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EcoPark.Infrastructure.Data;
+
+public static class DatabaseDbContextExtensions
+{
+    public static void AddDbContext(this IServiceCollection services, string connectionString)
+    {
+        services.AddDbContext<DatabaseDbContext>(options =>
+            options.UseSqlServer(connectionString));
+    }
+}
