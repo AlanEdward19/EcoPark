2c608856de0666cd865648a828cf41173193d226
Alan Oliveira - BDS DataSolution <aoliveira@bdsdatasolution.com.br>
2024-03-29 22:26:34 -0300

    feat: insert location command and handler

---
+++ src/EcoPark.Application/Locations/Insert/InsertLocationCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿namespace EcoPark.Application.Locations.Insert;
+
+public class InsertLocationCommandHandler(DatabaseDbContext databaseContext) : IHandler<InsertLocationCommand, DatabaseOperationResponseViewModel>
+{
+    public async Task<DatabaseOperationResponseViewModel> HandleAsync(InsertLocationCommand command, 
+        CancellationToken cancellationToken)
+    {
+        DatabaseOperationResponseViewModel result;
+        try
+        {
+            LocationModel locationModel = new(command.Name, command.Address);
+
+            await databaseContext.Locations.AddAsync(locationModel, cancellationToken);
+
+            await databaseContext.SaveChangesAsync(cancellationToken);
+
+            result = new DatabaseOperationResponseViewModel("Post" ,EOperationStatus.Successful, "Location was inserted successfully!");
+        }
+        catch (Exception e)
+        {
+            result = new DatabaseOperationResponseViewModel("Post", EOperationStatus.Failed, e.Message);
+        }
+
+        return result;
+    }
+}
