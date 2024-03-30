36bada226044c6965dd1a132f0d78db5c6c7ac50
Alan Oliveira - BDS DataSolution <aoliveira@bdsdatasolution.com.br>
2024-03-29 22:30:06 -0300

    feat: car viewModel

---
+++ src/EcoPark.Application/Cars/Models/CarViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace EcoPark.Application.Cars.Models;
+
+public class CarViewModel
+{
+    public string Plate { get; private set; }
+    public string Type { get; private set; }
+    public string Brand { get; private set; }
+    public string Model { get; private set; }
+    public string Color { get; private set; }
+    public int Year { get; private set; }
+}
