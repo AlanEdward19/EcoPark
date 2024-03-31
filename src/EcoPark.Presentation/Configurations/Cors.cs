5e9e4dd5127c4a3017feaabe3da95e4458eb6cc0
Alan Oliveira - BDS DataSolution <aoliveira@bdsdatasolution.com.br>
2024-03-31 16:33:27 -0300

    feat: cors configuration

---
+++ src/EcoPark.Presentation/Configurations/Cors.cs
@@ -0,0 +1,19 @@
+﻿namespace EcoPark.Presentation.Configurations;
+
+public static class Cors
+{
+    public static IServiceCollection ConfigureCors(this IServiceCollection services)
+    {
+        services.AddCors(options =>
+        {
+            options.AddPolicy("CorsPolicy", builder =>
+            {
+                builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader();
+            });
+        });
+
+        return services;
+    }
+}
