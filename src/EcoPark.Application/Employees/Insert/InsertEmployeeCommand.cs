63eeec15650d728d065805f896e97045fa4ed381
Alan Oliveira - BDS DataSolution <aoliveira@bdsdatasolution.com.br>
2024-03-31 16:31:27 -0300

    feat: added toLower to all email related function

--- src/EcoPark.Application/Employees/Insert/InsertEmployeeCommand.cs
+++ src/EcoPark.Application/Employees/Insert/InsertEmployeeCommand.cs
@@ -2,7 +2,7 @@
 
 public class InsertEmployeeCommand(string email, string password, string firstName, string lastName, EUserType userType)
 {
-    public string Email { get; private set; } = email;
+    public string Email { get; private set; } = email.ToLower();
     public string Password { get; private set; } = password;
     public string FirstName { get; private set; } = firstName;
     public string LastName { get; private set; } = lastName;
--- src/EcoPark.Application/Employees/Update/UpdateEmployeeCommand.cs
+++ src/EcoPark.Application/Employees/Update/UpdateEmployeeCommand.cs
@@ -5,7 +5,7 @@ public class UpdateEmployeeCommand(string? firstName, string? lastName, string? 
     public Guid EmployeeId { get; private set; }
     public string? FirstName { get; private set; } = firstName;
     public string? LastName { get; private set; } = lastName;
-    public string? Email { get; private set; } = email;
+    public string? Email { get; private set; } = email.ToLower();
     public string? Password { get; private set; } = password;
     public EUserType? UserType { get; private set; } = userType;
 
