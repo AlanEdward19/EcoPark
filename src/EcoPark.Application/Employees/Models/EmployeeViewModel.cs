b721a3932b5141cd25ecc1698343df084f42feca
Alan Oliveira - BDS DataSolution <aoliveira@bdsdatasolution.com.br>
2024-03-31 15:37:44 -0300

    feat: employee viewModel

--- src/EcoPark.Application/Employees/Models/EmployeeViewModel.cs
+++ src/EcoPark.Application/Employees/Models/EmployeeViewModel.cs
@@ -1,6 +1,9 @@
 ﻿namespace EcoPark.Application.Employees.Models;
 
-public class EmployeeViewModel
+public class EmployeeViewModel(string email, string firstName, string lastName, EUserType userType)
 {
-    
+    public string Email { get; private set; } = email;
+    public string FirstName { get; private set; } = firstName;
+    public string LastName { get; private set; } = lastName;
+    public string UserType { get; private set; } = userType.ToString();
 }
