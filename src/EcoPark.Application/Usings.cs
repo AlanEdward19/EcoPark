fe074ad4cb6694ae40506222df5b22079c679934
Alan Oliveira - BDS DataSolution <aoliveira@bdsdatasolution.com.br>
2024-03-31 15:39:41 -0300

    usings

--- src/EcoPark.Application/Usings.cs
+++ src/EcoPark.Application/Usings.cs
@@ -41,6 +41,12 @@ global using EcoPark.Application.Reservations.Get;
 global using EcoPark.Application.Reservations.Insert;
 global using EcoPark.Application.Reservations.List;
 global using EcoPark.Application.Reservations.Update;
+global using EcoPark.Application.Employees.Delete;
+global using EcoPark.Application.Employees.Get;
+global using EcoPark.Application.Employees.Insert;
+global using EcoPark.Application.Employees.List;
+global using EcoPark.Application.Employees.Update;
+global using EcoPark.Application.Authentication;
 
 global using EcoPark.Application.Commons.Base.Commands;
 
