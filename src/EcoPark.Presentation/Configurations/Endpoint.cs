7b54405ff3ed393c0046b510f0e802d169b97b51
Alan Oliveira - BDS DataSolution <aoliveira@bdsdatasolution.com.br>
2024-03-31 16:33:41 -0300

    feat: endpoint and middleware configurations

---
+++ src/EcoPark.Presentation/Configurations/Endpoint.cs
@@ -0,0 +1,19 @@
+﻿namespace EcoPark.Presentation.Configurations;
+
+public static class Endpoint
+{
+    public static IApplicationBuilder ConfigureEndpoints(this IApplicationBuilder app, IConfigurationSection section)
+    {
+        var apiHealthCheckUrl = section["APIHealthCheckUrl"];
+
+        app
+            .UseRouting()
+            .UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+                endpoints.MapHealthChecks(apiHealthCheckUrl);
+            });
+
+        return app;
+    }
+}
---
+++ src/EcoPark.Presentation/Configurations/ErrorHandling.cs
@@ -0,0 +1,13 @@
+﻿using EcoPark.Presentation.Middlewares;
+
+namespace EcoPark.Presentation.Configurations;
+
+public static class ErrorHandling
+{
+    public static IApplicationBuilder ConfigureMiddleware(this IApplicationBuilder app)
+    {
+        app.UseMiddleware<ExceptionMiddleware>();
+
+        return app;
+    }
+}
