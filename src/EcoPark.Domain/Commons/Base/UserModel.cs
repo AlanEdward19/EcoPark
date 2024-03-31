f0c92fa5beb63c44fa785ba6869d6dee07e65b26
Alan Oliveira - BDS DataSolution <aoliveira@bdsdatasolution.com.br>
2024-03-31 15:38:37 -0300

    feat: userModel properties made public for update

--- src/EcoPark.Domain/Commons/Base/UserModel.cs
+++ src/EcoPark.Domain/Commons/Base/UserModel.cs
@@ -3,8 +3,8 @@
 public class UserModel(string email, string password, string firstName, string lastName)
     : BaseDataModel
 {
-    public string Email { get; private set; } = email;
-    public string Password { get; private set; } = password;
-    public string FirstName { get; private set; } = firstName;
-    public string LastName { get; private set; } = lastName;
+    public string Email { get; set; } = email;
+    public string Password { get; set; } = password;
+    public string FirstName { get; set; } = firstName;
+    public string LastName { get; set; } = lastName;
 }
