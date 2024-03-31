5ddfc75333d3ed58f12e51df7e8353d7be3e900b
Alan Oliveira - BDS DataSolution <aoliveira@bdsdatasolution.com.br>
2024-03-31 15:38:16 -0300

    feat: employee model method to update info based on valueObject

--- src/EcoPark.Domain/DataModels/EmployeeModel.cs
+++ src/EcoPark.Domain/DataModels/EmployeeModel.cs
@@ -1,7 +1,18 @@
-﻿namespace EcoPark.Domain.DataModels;
+﻿using EcoPark.Domain.ValueObjects;
+
+namespace EcoPark.Domain.DataModels;
 
 public class EmployeeModel(string email, string password, string firstName, string lastName, EUserType userType) 
     : UserModel(email, password, firstName, lastName)
 {
-    public EUserType UserType { get; private set; }
+    public EUserType UserType { get; private set; } = userType;
+
+    public void UpdateBasedOnValueObject(EmployeeValueObject employeeValueObject)
+    {
+        Email = employeeValueObject.Email;
+        Password = employeeValueObject.Password;
+        FirstName = employeeValueObject.FirstName;
+        LastName = employeeValueObject.LastName;
+        UserType = employeeValueObject.UserType;
+    }
 }
