827439f0bc6ee602bab8d471c4efbcd1ea0afc02
Alan Oliveira - BDS DataSolution <aoliveira@bdsdatasolution.com.br>
2024-03-31 16:31:11 -0300

    feat: authentication query, handler and viewModel

---
+++ src/EcoPark.Application/Authentication/Get/LoginQuery.cs
@@ -0,0 +1,10 @@
+﻿namespace EcoPark.Application.Authentication.Get;
+
+public class LoginQuery(string email, string password)
+{
+    public string Email { get; private set; } = email;
+    public string Password { get; private set; } = password;
+    public bool IsEmployee { get; private set; }
+
+    public void SetIsEmployee(bool isEmployee) => IsEmployee = isEmployee;
+}
---
+++ src/EcoPark.Application/Authentication/Get/LoginQueryHandler.cs
@@ -0,0 +1,43 @@
+﻿using EcoPark.Application.Authentication.Models;
+
+namespace EcoPark.Application.Authentication.Get;
+
+public class LoginQueryHandler
+(DatabaseDbContext databaseDbContext,
+    IAuthenticationService authenticationService) : IHandler<LoginQuery, LoginViewModel>
+{
+    public async Task<LoginViewModel> HandleAsync(LoginQuery command, CancellationToken cancellationToken)
+    {
+        LoginViewModel result;
+
+        string hashedPassword = authenticationService.ComputeSha256Hash(command.Password);
+
+        if (command.IsEmployee)
+        {
+            EmployeeModel? employeeModel = await databaseDbContext.Employees
+                .FirstOrDefaultAsync(e => e.Email == command.Email && e.Password == hashedPassword, cancellationToken);
+
+            if (employeeModel == null)
+                throw new Exception("User not found!");
+
+            string token = authenticationService.GenerateJwtToken(command.Email, employeeModel.UserType.ToString());
+
+            result = new LoginViewModel(employeeModel.Email, token);
+        }
+
+        else
+        {
+            ClientModel? clientModel = await databaseDbContext.Clients
+                .FirstOrDefaultAsync(c => c.Email == command.Email && c.Password == hashedPassword, cancellationToken);
+
+            if (clientModel == null)
+                throw new Exception("User not found!");
+
+            string token = authenticationService.GenerateJwtToken(command.Email, "Client");
+
+            result = new LoginViewModel(clientModel.Email, token);
+        }
+
+        return result;
+    }
+}
