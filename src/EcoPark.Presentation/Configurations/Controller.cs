223e8c5fad4cbd59c11bbde4020d7eeb7958ea7a
Alan Oliveira - BDS DataSolution <aoliveira@bdsdatasolution.com.br>
2024-03-31 16:33:20 -0300

    feat: controller configuration

---
+++ src/EcoPark.Presentation/Configurations/Controller.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Json.Serialization;
+
+namespace EcoPark.Presentation.Configurations;
+
+public static class Controller
+{
+    public static IServiceCollection ConfigureController(this IServiceCollection services)
+    {
+        //services
+        //    .AddControllers()
+        //    .AddFluentValidation(c =>
+        //    {
+        //        c.RegisterValidatorsFromAssemblyContaining<CreateInstructorCommandValidator>();
+        //        c.ValidatorOptions.DefaultClassLevelCascadeMode = CascadeMode.Stop;
+        //    });
+
+        services.AddControllersWithViews()
+            .AddJsonOptions(options =>
+            {
+                options.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
+            });
+
+        return services;
+    }
+}
