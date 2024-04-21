526a1603494becfaecb3dffab90ba38b4474d1c8
Alan Oliveira - BDS DataSolution <aoliveira@bdsdatasolution.com.br>
2024-04-21 13:32:13 -0300

    feat: repositories and uow implemented

--- src/EcoPark.Infrastructure/InfrastructureModule.cs
+++ src/EcoPark.Infrastructure/InfrastructureModule.cs
@@ -1,4 +1,8 @@
-﻿using EcoPark.Infrastructure.Data;
+﻿using EcoPark.Application.Clients.Models;
+using EcoPark.Domain.Commons.Base;
+using EcoPark.Domain.Interfaces.Database;
+using EcoPark.Infrastructure.Data;
+using EcoPark.Infrastructure.Repositories;
 using Microsoft.Extensions.Configuration;
 
 namespace EcoPark.Infrastructure;
@@ -8,7 +12,9 @@ public static class InfrastructureModule
     public static IServiceCollection ConfigureInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
         services
-            .ConfigureDatabase(configuration);
+            .ConfigureDatabase(configuration)
+            .ConfigureWebSocket()
+            .ConfigureRepositories();
 
         return services;
     }
@@ -17,6 +23,28 @@ public static class InfrastructureModule
     {
         services
             .AddDbContext(configuration.GetConnectionString("MainDatabase"));
+
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+        return services;
+    }
+
+    private static IServiceCollection ConfigureRepositories(this IServiceCollection services)
+    {
+        services.AddScoped<IAggregateRepository<LocationModel>, LocationRepository>();
+        services.AddScoped<IAggregateRepository<ParkingSpaceModel>, ParkingSpaceRepository>();
+        services.AddScoped<IRepository<EmployeeModel>, EmployeeRepository>();
+        services.AddScoped<IRepository<ReservationModel>, ReservationRepository>();
+        services.AddScoped<IRepository<UserModel>, LoginRepository>();
+        services.AddScoped<IRepository<ClientSimplifiedViewModel>, ClientRepository>();
+
+        return services;
+    }
+
+    private static IServiceCollection ConfigureWebSocket(this IServiceCollection services)
+    {
+        services
+            .AddSignalR();
 
         return services;
     }
--- src/EcoPark.Infrastructure/Usings.cs
+++ src/EcoPark.Infrastructure/Usings.cs
@@ -1,7 +1,25 @@
 ﻿global using Microsoft.Extensions.DependencyInjection;
+global using Microsoft.EntityFrameworkCore;
 
 #region Domain
 
 global using EcoPark.Domain.DataModels;
+global using EcoPark.Domain.Commons.Base;
+global using EcoPark.Domain.Interfaces;
+global using EcoPark.Domain.Interfaces.Database;
+global using EcoPark.Domain.Interfaces.Services;
+global using EcoPark.Domain.ValueObjects;
+
+#endregion
+
+#region Application
+
+global using EcoPark.Application.Authentication.Get;
+
+#endregion
+
+#region Infrastructure
+
+global using EcoPark.Infrastructure.Data;
 
 #endregion
