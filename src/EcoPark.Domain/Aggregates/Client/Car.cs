20b177480656b193cb9092cf8364c5992bd9e9c3
Alan Oliveira - BDS DataSolution <aoliveira@bdsdatasolution.com.br>
2024-04-23 12:23:07 -0300

    feat: added validation to check empty values

--- src/EcoPark.Domain/Aggregates/Client/Car.cs
+++ src/EcoPark.Domain/Aggregates/Client/Car.cs
@@ -1,24 +1,48 @@
 ﻿namespace EcoPark.Domain.Aggregates.Client;
 
-public class Car
+public class Car(CarModel carModel)
 {
-    public Guid Id { get; private set; }
-    public string Plate { get; private set; }
-    public ECarType Type { get; private set; }
-    public string Model { get; private set; }
-    public string Color { get; private set; }
-    public string Brand { get; private set; }
-    public int Year { get; private set; }
+    public Guid Id { get; private set; } = carModel.Id;
+    public string Plate { get; private set; } = carModel.Plate;
+    public ECarType Type { get; private set; } = carModel.Type;
+    public string Model { get; private set; } = carModel.Model;
+    public string Color { get; private set; } = carModel.Color;
+    public string Brand { get; private set; } = carModel.Brand;
+    public int Year { get; private set; } = carModel.Year;
 
-    public void UpdatePlate(string plate) => Plate = plate;
+    public void UpdatePlate(string? plate)
+    {
+        if (!string.IsNullOrWhiteSpace(plate) && plate.Equals(Plate, StringComparison.InvariantCultureIgnoreCase))
+            Plate = plate;
+    }
 
-    public void UpdateType(ECarType type) => Type = type;
+    public void UpdateType(ECarType? type)
+    {
+        if(type != null && type != Type) 
+            Type = type.Value;
+    }
 
-    public void UpdateModel(string model) => Model = model;
+    public void UpdateModel(string? model)
+    {
+        if(!string.IsNullOrWhiteSpace(model) && model.Equals(Model, StringComparison.InvariantCultureIgnoreCase)) 
+            Model = model;
+    }
 
-    public void UpdateColor(string color) => Color = color;
+    public void UpdateColor(string? color)
+    {
+        if (!string.IsNullOrWhiteSpace(color) && color.Equals(Color, StringComparison.InvariantCultureIgnoreCase))
+            Color = color;
+    }
 
-    public void UpdateBrand(string brand) => Brand = brand;
+    public void UpdateBrand(string? brand)
+    {
+        if (!string.IsNullOrWhiteSpace(brand) && brand.Equals(Brand, StringComparison.InvariantCultureIgnoreCase))
+            Brand = brand;
+    }
 
-    public void UpdateYear(int year) => Year = year;
+    public void UpdateYear(int? year)
+    {
+        if (year != null && Year != year)
+            Year = year.Value;
+    }
 }
