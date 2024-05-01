07e2f3b3063e1449fdab460144c60355d36ce5d5
Alan Oliveira - BDS DataSolution <aoliveira@bdsdatasolution.com.br>
2024-05-01 18:13:38 -0300

    feat: filter to ignore specific property on swagger build

---
+++ src/EcoPark.Presentation/Filters/IgnorePropertiesOperationFilter.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace EcoPark.Presentation.Filters;
+
+public class IgnorePropertiesOperationFilter : IOperationFilter
+{
+    public void Apply(OpenApiOperation operation, OperationFilterContext context)
+    {
+        var ignoredProperties = new[] { "RequestUserInfo" };
+
+        foreach (var parameter in operation.Parameters.ToArray())
+        {
+            if (ignoredProperties.Contains(parameter.Name))
+            {
+                operation.Parameters.Remove(parameter);
+            }
+        }
+    }
+}
