42e611e09464301ab32420e451c25d5d8aa52580
Alan Oliveira - BDS DataSolution <aoliveira@bdsdatasolution.com.br>
2024-05-01 17:38:33 -0300

    feat: added command and handler for permission to access location to employee

---
+++ src/EcoPark.Application/Employees/Insert/InsertEmployeeGroupAccessCommand.cs
@@ -0,0 +1,14 @@
+﻿namespace EcoPark.Application.Employees.Insert;
+
+public class InsertEmployeeGroupAccessCommand : ICommand
+{
+    public Guid EmployeeId { get; set; }
+    public Guid LocationId { get; set; }
+
+    public (string Email, EUserType UserType) RequestUserInfo { get; private set; }
+
+    public void SetRequestUserInfo((string email, EUserType userType) information)
+    {
+        RequestUserInfo = information;
+    }
+}
---
+++ src/EcoPark.Application/Employees/Insert/InsertEmployeeGroupAccessCommandHandler.cs
@@ -0,0 +1,41 @@
+﻿namespace EcoPark.Application.Employees.Insert;
+
+public class InsertEmployeeGroupAccessCommandHandler(IRepository<EmployeeModel> repository) : IHandler<InsertEmployeeGroupAccessCommand, DatabaseOperationResponseViewModel>
+{
+    public async Task<DatabaseOperationResponseViewModel> HandleAsync(InsertEmployeeGroupAccessCommand command, CancellationToken cancellationToken)
+    {
+        DatabaseOperationResponseViewModel result;
+
+        try
+        {
+            if (await repository.CheckChangePermissionAsync(command, CancellationToken.None))
+            {
+                await repository.UnitOfWork.StartAsync(cancellationToken);
+
+                var databaseOperationResult = await repository.AddAsync(command, cancellationToken);
+
+                if (databaseOperationResult)
+                {
+                    await repository.UnitOfWork.SaveEntitiesAsync(cancellationToken);
+                    await repository.UnitOfWork.CommitAsync(cancellationToken);
+
+                    result = new DatabaseOperationResponseViewModel("Post", EOperationStatus.Successful, "Employee received access to location successfully");
+                }
+                else
+                {
+                    await repository.UnitOfWork.RollbackAsync(cancellationToken);
+                    result = new DatabaseOperationResponseViewModel("Post", EOperationStatus.Failed, "Employee didn't receive access to location");
+                }
+            }
+            else
+                result = new DatabaseOperationResponseViewModel("Post", EOperationStatus.Failed, "You don't have permission to give access to this location");
+        }
+        catch (Exception e)
+        {
+            await repository.UnitOfWork.RollbackAsync(cancellationToken);
+            result = new DatabaseOperationResponseViewModel("Post", EOperationStatus.Failed, e.Message);
+        }
+
+        return result;
+    }
+}
