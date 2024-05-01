f132581638441037c8219f478547320261337875
Alan Oliveira - BDS DataSolution <aoliveira@bdsdatasolution.com.br>
2024-05-01 17:57:33 -0300

    feat: command and handler to remove permission

---
+++ src/EcoPark.Application/Employees/Delete/GroupAccess/DeleteEmployeeGroupAccessCommand.cs
@@ -0,0 +1,13 @@
+﻿namespace EcoPark.Application.Employees.Delete.GroupAccess;
+
+public class DeleteEmployeeGroupAccessCommand : ICommand
+{
+    public Guid LocationId { get; set; }
+    public Guid EmployeeId { get; set; }
+
+    public (string Email, EUserType UserType) RequestUserInfo { get; private set; }
+    public void SetRequestUserInfo((string email, EUserType userType) information)
+    {
+        RequestUserInfo = information;
+    }
+}
