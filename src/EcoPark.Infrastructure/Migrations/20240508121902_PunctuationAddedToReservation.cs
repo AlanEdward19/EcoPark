0c8d46ce89020d935d1b1259299e626eba46fe20
Alan Oliveira - BDS DataSolution <aoliveira@bdsdatasolution.com.br>
2024-05-08 11:00:35 -0300

    chore: migrations

---
+++ src/EcoPark.Infrastructure/Migrations/20240508121902_PunctuationAddedToReservation.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EcoPark.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class PunctuationAddedToReservation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "Punctuation",
+                table: "Reservations",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Punctuation",
+                table: "Reservations");
+        }
+    }
+}
