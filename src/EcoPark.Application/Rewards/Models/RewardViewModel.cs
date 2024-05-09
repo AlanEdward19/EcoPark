c273b3fef0fa1c2a6cd02c38f0c99990e60957c8
Alan Oliveira - BDS DataSolution <aoliveira@bdsdatasolution.com.br>
2024-05-09 20:11:26 -0300

    feat: rewards model

---
+++ src/EcoPark.Application/Rewards/Models/RewardViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace EcoPark.Application.Rewards.Models;
+
+public class RewardViewModel(string name, string description, int? quantity, double pointCost, string imageUrl, DateTime? expirationDate)
+{
+    public string Name { get; private set; } = name;
+    public string Description { get; private set; } = description;
+    public int? Quantity { get; private set; } = quantity;
+    public double PointCost { get; private set; } = pointCost;
+    public string ImageUrl { get; private set; } = imageUrl;
+    public DateTime? ExpirationDate { get; private set; } = expirationDate;
+}
