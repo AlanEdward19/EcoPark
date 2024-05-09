a16bcd82650d5b210c3cf1306829c54f9cf21bc1
Alan Oliveira - BDS DataSolution <aoliveira@bdsdatasolution.com.br>
2024-05-09 20:12:09 -0300

    feat: added missing validation and repository methods for delete, get and list

--- src/EcoPark.Infrastructure/Repositories/RewardRepository.cs
+++ src/EcoPark.Infrastructure/Repositories/RewardRepository.cs
@@ -1,4 +1,7 @@
-﻿using EcoPark.Application.Rewards.Insert;
+﻿using EcoPark.Application.Rewards.Delete;
+using EcoPark.Application.Rewards.Get;
+using EcoPark.Application.Rewards.Insert;
+using EcoPark.Application.Rewards.List;
 using EcoPark.Application.Rewards.Update;
 using EcoPark.Domain.Interfaces.Providers;
 
@@ -10,7 +13,108 @@ public class RewardRepository(DatabaseDbContext databaseDbContext, IUnitOfWork u
 
     public async Task<bool> CheckChangePermissionAsync(ICommand command, CancellationToken cancellationToken)
     {
-        return true; //fazer validações
+        EmployeeModel? owner;
+        LocationModel? location;
+        RewardModel? reward;
+        var requestUserInfo = command.RequestUserInfo;
+
+        switch (command)
+        {
+            case InsertRewardCommand insertCommand:
+                location = await databaseDbContext.Locations
+                    .AsNoTracking()
+                    .FirstOrDefaultAsync(x => x.Id.Equals(insertCommand.LocationId.Value), cancellationToken);
+
+                if (location == null) return false;
+
+                owner = await databaseDbContext.Employees
+                    .Include(x => x.Credentials)
+                    .Include(x => x.Employees)
+                    .ThenInclude(x => x.Credentials)
+                    .AsNoTracking()
+                    .AsSplitQuery()
+                    .FirstOrDefaultAsync(x => x.Id.Equals(location.OwnerId), cancellationToken);
+
+                if (owner == null) return false;
+
+                if (owner.Credentials.Email.Equals(requestUserInfo.Email)) return true;
+
+                if (owner.Employees.Select(x => x.Credentials.Email).Contains(requestUserInfo.Email))
+                {
+                    EmployeeModel employeeModel = (await databaseDbContext.Employees
+                        .Include(x => x.Credentials)
+                        .Include(x => x.GroupAccesses)
+                        .FirstOrDefaultAsync(x => x.Credentials.Email.Equals(requestUserInfo.Email), cancellationToken))!;
+
+                    return employeeModel.GroupAccesses.Any(x => x.LocationId.Equals(location.Id));
+                }
+
+                break;
+
+            case UpdateRewardCommand updateCommand:
+                reward = await databaseDbContext.Rewards
+                    .Include(x => x.Location)
+                    .FirstOrDefaultAsync(x => x.Id.Equals(updateCommand.Id), cancellationToken);
+
+                if (reward == null) return false;
+
+                owner = await databaseDbContext.Employees
+                    .Include(x => x.Credentials)
+                    .Include(x => x.Employees)
+                    .ThenInclude(x => x.Credentials)
+                    .AsNoTracking()
+                    .AsSplitQuery()
+                    .FirstOrDefaultAsync(x => x.Id.Equals(reward.Location.OwnerId), cancellationToken);
+
+                if (owner == null) return false;
+
+                if (owner.Credentials.Email.Equals(requestUserInfo.Email)) return true;
+
+                if (owner.Employees.Select(x => x.Credentials.Email).Contains(requestUserInfo.Email))
+                {
+                    EmployeeModel employeeModel = (await databaseDbContext.Employees
+                        .Include(x => x.Credentials)
+                        .Include(x => x.GroupAccesses)
+                        .FirstOrDefaultAsync(x => x.Credentials.Email.Equals(requestUserInfo.Email), cancellationToken))!;
+
+                    return employeeModel.GroupAccesses.Any(x => x.LocationId.Equals(reward.Location.Id));
+                }
+
+                break;
+
+            case DeleteRewardCommand deleteCommand:
+                reward = await databaseDbContext.Rewards
+                    .Include(x => x.Location)
+                    .FirstOrDefaultAsync(x => x.Id.Equals(deleteCommand.Id), cancellationToken);
+
+                if (reward == null) return false;
+
+                owner = await databaseDbContext.Employees
+                    .Include(x => x.Credentials)
+                    .Include(x => x.Employees)
+                    .ThenInclude(x => x.Credentials)
+                    .AsNoTracking()
+                    .AsSplitQuery()
+                    .FirstOrDefaultAsync(x => x.Id.Equals(reward.Location.OwnerId), cancellationToken);
+
+                if (owner == null) return false;
+
+                if (owner.Credentials.Email.Equals(requestUserInfo.Email)) return true;
+
+                if (owner.Employees.Select(x => x.Credentials.Email).Contains(requestUserInfo.Email))
+                {
+                    EmployeeModel employeeModel = (await databaseDbContext.Employees
+                        .Include(x => x.Credentials)
+                        .Include(x => x.GroupAccesses)
+                        .FirstOrDefaultAsync(x => x.Credentials.Email.Equals(requestUserInfo.Email), cancellationToken))!;
+
+                    return employeeModel.GroupAccesses.Any(x => x.LocationId.Equals(reward.Location.Id));
+                }
+
+                break;
+        }
+
+        return false;
     }
 
     public async Task<bool> AddAsync(ICommand command, CancellationToken cancellationToken)
@@ -81,16 +185,44 @@ public class RewardRepository(DatabaseDbContext databaseDbContext, IUnitOfWork u
 
     public async Task<bool> DeleteAsync(ICommand command, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        var parsedCommand = command as DeleteRewardCommand;
+
+        RewardModel? reward = await databaseDbContext.Rewards
+            .Include(x => x.Location)
+            .FirstOrDefaultAsync(e => e.Id == parsedCommand.Id, cancellationToken);
+
+        if (reward == null) return false;
+
+        await storageProvider.DeleteBlobAsync(reward.Image, "rewards");
+        databaseDbContext.Rewards.Remove(reward);
+
+        return true;
     }
 
     public async Task<RewardModel?> GetByIdAsync(IQuery query, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        var parsedQuery = query as GetRewardQuery;
+
+        return await databaseDbContext.Rewards
+            .FirstOrDefaultAsync(e => e.Id == parsedQuery.RewardId, cancellationToken);
     }
 
     public async Task<IEnumerable<RewardModel>> ListAsync(IQuery query, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        var parsedQuery = query as ListRewardsQuery;
+
+        IQueryable<RewardModel> databaseQuery = databaseDbContext.Rewards
+            .Include(x => x.Location)
+            .AsQueryable()
+            .AsSplitQuery()
+            .AsNoTracking();
+
+        if (parsedQuery.LocationId != null)
+            databaseQuery = databaseQuery.Where(e => e.LocationId == parsedQuery.LocationId);
+
+        if (parsedQuery.RewardIds != null && parsedQuery.RewardIds.Any())
+            databaseQuery = databaseQuery.Where(e => parsedQuery.RewardIds.Contains(e.Id));
+
+        return await databaseQuery.ToListAsync(cancellationToken);
     }
 }
