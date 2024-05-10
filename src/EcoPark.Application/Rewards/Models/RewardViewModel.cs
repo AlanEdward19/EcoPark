bbe4c3d855e2ecf2580dede73f43fa47bd25951f
Alan Oliveira - BDS DataSolution <aoliveira@bdsdatasolution.com.br>
2024-05-10 10:53:41 -0300

    chore: splitted viewModel

--- src/EcoPark.Application/Rewards/Models/RewardViewModel.cs
+++ src/EcoPark.Application/Rewards/Models/RewardViewModel.cs
@@ -1,11 +1,8 @@
 ﻿namespace EcoPark.Application.Rewards.Models;
 
-public class RewardViewModel(string name, string description, int? quantity, double pointCost, string imageUrl, DateTime? expirationDate)
+public class RewardViewModel(Guid id, string name, string description, int? quantity, double pointCost, string url, string imageUrl, DateTime? expirationDate) : RewardSimplifiedViewModel(name, description, url, imageUrl, expirationDate)
 {
-    public string Name { get; private set; } = name;
-    public string Description { get; private set; } = description;
+    public Guid Id { get; private set; } = id;
     public int? Quantity { get; private set; } = quantity;
     public double PointCost { get; private set; } = pointCost;
-    public string ImageUrl { get; private set; } = imageUrl;
-    public DateTime? ExpirationDate { get; private set; } = expirationDate;
 }
