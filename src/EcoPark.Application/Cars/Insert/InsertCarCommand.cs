cc78c884b8f5c364a3e9864ece81749e25082b9f
Alan Oliveira - BDS DataSolution <aoliveira@bdsdatasolution.com.br>
2024-05-18 19:09:19 -0300

    feat: added fuelType and fuelConsumption to car model and commands

--- src/EcoPark.Application/Cars/Insert/InsertCarCommand.cs
+++ src/EcoPark.Application/Cars/Insert/InsertCarCommand.cs
@@ -1,6 +1,7 @@
 ﻿namespace EcoPark.Application.Cars.Insert;
 
-public class InsertCarCommand(string? plate, ECarType? type, string? model, string? brand, string? color, int? year) : ICommand
+public class InsertCarCommand(string? plate, ECarType? type, string? model, string? brand, string? color, 
+    int? year, EFuelType? fuelType, double? fuelConsumptionPerLiter) : ICommand
 {
     public string? Plate { get; private set; } = plate;
     public ECarType? Type { get; private set; } = type;
@@ -8,10 +9,13 @@ public class InsertCarCommand(string? plate, ECarType? type, string? model, stri
     public string? Brand { get; private set; } = brand;
     public string? Color { get; private set; } = color;
     public int? Year { get; private set; } = year;
+    public EFuelType? FuelType { get; private set; } = fuelType;
+    public double? FuelConsumptionPerLiter { get; private set; } = fuelConsumptionPerLiter;
 
     public CarModel ToModel(Guid clientId)
     {
-        return new(clientId, Plate!, Type!.Value, Model!, Color!, Brand!, Year!.Value);
+        return new(clientId, Plate!, Type!.Value, Model!, Color!, Brand!, Year!.Value, FuelType!.Value,
+            FuelConsumptionPerLiter!.Value);
     }
 
     [JsonIgnore]
--- src/EcoPark.Domain/DataModels/Client/CarModel.cs
+++ src/EcoPark.Domain/DataModels/Client/CarModel.cs
@@ -1,6 +1,6 @@
 ﻿namespace EcoPark.Domain.DataModels.Client;
 
-public class CarModel(Guid clientId, string plate, ECarType type, string model, string color, string brand, int year)
+public class CarModel(Guid clientId, string plate, ECarType type, string model, string color, string brand, int year, EFuelType fuelType, double fuelConsumptionPerLiter)
     : BaseDataModel
 {
     public Guid ClientId { get; set; } = clientId;
@@ -10,6 +10,8 @@ public class CarModel(Guid clientId, string plate, ECarType type, string model, 
     public string Color { get; set; } = color;
     public string Brand { get; set; } = brand;
     public int Year { get; set; } = year;
+    public EFuelType FuelType { get; set; } = fuelType;
+    public double FuelConsumptionPerLiter { get; set; } = fuelConsumptionPerLiter;
 
     [ForeignKey(nameof(ClientId))]
     public virtual ClientModel Client { get; set; }
@@ -24,6 +26,8 @@ public class CarModel(Guid clientId, string plate, ECarType type, string model, 
         Color = car.Color;
         Brand = car.Brand;
         Year = car.Year;
+        FuelConsumptionPerLiter = car.FuelConsumptionPerLiter;
+        FuelType = car.FuelType;
         UpdatedAt = DateTime.Now;
     }
 }
