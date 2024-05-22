75582038fbe029070482d39b7f28f86071c6230a
Alan Oliveira - BDS DataSolution <aoliveira@bdsdatasolution.com.br>
2024-05-22 09:56:46 -0300

    fix: removed private property to showing on body request

--- src/EcoPark.Application/Authentication/Get/LoginQuery.cs
+++ src/EcoPark.Application/Authentication/Get/LoginQuery.cs
@@ -5,6 +5,7 @@ public class LoginQuery(string email, string password) : IQuery
     public string Email { get; private set; } = email;
     public string Password { get; private set; } = password;
 
+    [JsonIgnore]
     public (string Email, EUserType UserType) RequestUserInfo { get; private set; }
     public void SetRequestUserInfo((string email, EUserType userType) information)
     {
